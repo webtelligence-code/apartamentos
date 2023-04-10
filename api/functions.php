155af5b9fe575d37bed518eb8f66429a57db10ff
webtelligence-code <manuelbcarreiras@gmail.com>
2023-04-10 12:44:58 +0100

    First Commit

---
+++ api/functions.php
@@ -0,0 +1,238 @@
+<?php
+
+include 'DatabaseConnect.php';
+
+include 'session.php';
+
+$databaseObj = new DatabaseConnect;
+$conn = $databaseObj->connect();
+
+// Function that will fetch session username
+function getUsername()
+{
+    return $_SESSION['USERNAME'];
+}
+
+/**
+ * Function to fetch all meetings from database
+ * We need to join the salas table to reunioes table
+ * @return void 
+ */
+function getMeetings()
+{
+    global $conn;
+    $sql = 'SELECT reunioes.id, reunioes.motivo, reunioes.data, reunioes.hora_inicio, reunioes.hora_fim, reunioes.organizador, salas.nome 
+            AS sala, salas.url_imagem,
+            (
+                SELECT GROUP_CONCAT(participantes.nome_participante SEPARATOR \', \')
+                FROM participantes
+                WHERE participantes.id_reuniao = reunioes.id
+            ) AS participantes
+            FROM reunioes
+            JOIN salas ON reunioes.id_sala = salas.id
+            ORDER BY reunioes.data DESC, reunioes.hora_inicio ASC
+    ';
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $meetings = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($meetings as $key => $meeting) {
+        $meetings[$key]['participantes'] = explode(',', $meeting['participantes']);
+    }
+
+    return $meetings;
+}
+
+// Function that will fetch all rooms available in the database
+function getRooms()
+{
+    global $conn;
+    $sql = 'SELECT * FROM salas';
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $rooms = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    return $rooms;
+}
+
+// Function that will fetch all users (guests) from the database
+function getUsers()
+{
+    global $conn;
+    $sql = 'SELECT * FROM users ORDER BY NAME ASC';
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    return $users;
+}
+
+function checkMeetingConflict($id_sala, $data, $hora_inicio, $hora_fim, $id_reuniao)
+{
+    global $conn;
+
+    $sql = 'SELECT * FROM reunioes
+            WHERE id_sala = :id_sala
+            AND data = :data
+            AND ((:hora_inicio >= hora_inicio AND :hora_inicio < hora_fim) OR (hora_inicio >= :hora_inicio AND hora_inicio < :hora_fim))';
+
+    if ($id_reuniao) {
+        $sql .= ' AND id != :id_reuniao';
+    }
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':id_sala', $id_sala);
+    $stmt->bindParam(':data', $data);
+    $stmt->bindParam(':hora_inicio', $hora_inicio);
+    $stmt->bindParam(':hora_fim', $hora_fim);
+
+    if ($id_reuniao) {
+        $stmt->bindParam(':id_reuniao', $id_reuniao);
+    }
+
+    $stmt->execute();
+    $meetings = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    return count($meetings) > 0;
+}
+
+/**
+ * Function that will handle insert a new meeting into the database
+ * @param mixed $meeting 
+ * @return void 
+ * @throws PDOException 
+ */
+function addMeeting($meeting)
+{
+    global $conn;
+
+    $meetingsSql = 'INSERT INTO reunioes (motivo, data, hora_inicio, hora_fim, organizador, id_sala)
+                    VALUES (:motivo, :data, :hora_inicio, :hora_fim, :organizador, :sala)';
+    // Add the meeting to the reunioes table
+    $stmt = $conn->prepare($meetingsSql);
+    $stmt->bindParam(':motivo', $meeting['motivo']);
+    $stmt->bindParam(':data', $meeting['data']);
+    $stmt->bindParam(':hora_inicio', $meeting['hora_inicio']);
+    $stmt->bindParam(':hora_fim', $meeting['hora_fim']);
+    $stmt->bindParam(':organizador', $meeting['organizador']);
+    $stmt->bindParam(':sala', $meeting['sala']);
+    $stmt->execute();
+
+    // Get the id of the inserted meeting
+    $meeting_id = $conn->lastInsertId();
+
+    $guestsSql = 'INSERT INTO participantes (id_reuniao, nome_participante)
+                    VALUES (:id_reuniao, :nome_participante)';
+    // Add the guests to the participantes table
+    foreach ($meeting['participantes'] as $participante) {
+        $stmt = $conn->prepare($guestsSql);
+        $stmt->bindParam(':id_reuniao', $meeting_id);
+        $stmt->bindParam(':nome_participante', $participante);
+        $stmt->execute();
+    }
+
+    return [
+        'status' => 'success',
+        'message' => 'Reunião adicionada com sucesso!',
+        'title' => 'Sucesso!'
+    ];
+}
+
+/**
+ * This function will update the meeting in the database
+ * @param object $meeting 
+ * @return string[] response array
+ * @throws PDOException 
+ */
+function updateMeeting($meeting)
+{
+    global $conn;
+
+    $meetingSql = 'UPDATE reunioes 
+                    SET motivo = :motivo, data = :data, hora_inicio = :hora_inicio, hora_fim = :hora_fim, organizador = :organizador, id_sala = :sala 
+                    WHERE id = :id';
+    // Update the meeting in the reunioes table
+    $stmt = $conn->prepare($meetingSql);
+    $stmt->bindParam(':motivo', $meeting['motivo']);
+    $stmt->bindParam(':data', $meeting['data']);
+    $stmt->bindParam(':hora_inicio', $meeting['hora_inicio']);
+    $stmt->bindParam(':hora_fim', $meeting['hora_fim']);
+    $stmt->bindParam(':organizador', $meeting['organizador']);
+    $stmt->bindParam(':sala', $meeting['sala']);
+    $stmt->bindParam(':id', $meeting['meeting_id']);
+    $stmt->execute();
+
+    $deleteGuestsSql = 'DELETE FROM participantes WHERE id_reuniao = :id_reuniao';
+    // Delete the existing guests in the guests table
+    $stmt = $conn->prepare($deleteGuestsSql);
+    $stmt->bindParam(':id_reuniao', $meeting['meeting_id']);
+    $stmt->execute();
+
+    $updateGuestsSql = 'INSERT INTO participantes (id_reuniao, nome_participante) 
+                        VALUES (:id_reuniao, :nome_participante)';
+    // Add the updated guests to the guests table
+    foreach ($meeting['participantes'] as $participante) {
+        $stmt = $conn->prepare($updateGuestsSql);
+        $stmt->bindParam(':id_reuniao', $meeting['meeting_id']);
+        $stmt->bindParam(':nome_participante', $participante);
+        $stmt->execute();
+    }
+
+    return [
+        'status' => 'success',
+        'message' => 'Reunião atualizada com sucesso!',
+        'title' => 'Atualizada!'
+    ];
+}
+
+/**
+ * This function will handle a transaction to delete meeting in reunioes table
+ * and also delete all the participantes associated to the meeting in question
+ * @param object $meeting_id 
+ * @return bool|void 
+ * @throws PDOException 
+ */
+function deleteMeeting($meeting_id)
+{
+    global $conn;
+
+    try {
+        // Start a transaction
+        $conn->beginTransaction();
+
+        // Delete the associated guests
+        $guestsSql = 'DELETE FROM participantes WHERE id_reuniao = :meeting_id';
+        $stmt = $conn->prepare($guestsSql);
+        $stmt->bindParam(':meeting_id', $meeting_id, PDO::PARAM_INT);
+        $stmt->execute();
+
+        // Delete the meeting
+        $meetingsSql = 'DELETE FROM reunioes WHERE ID = :meeting_id';
+        $stmt = $conn->prepare($meetingsSql);
+        $stmt->bindParam(':meeting_id', $meeting_id, PDO::PARAM_INT);
+        $result = $stmt->execute();
+
+        // Commit the transaction
+        $conn->commit();
+
+        if ($result) {
+            $response = [
+                'status' => 'success',
+                'message' => 'Reunião removida com sucesso!',
+                'title' => 'Removida!'
+            ];
+        } else {
+            $response = [
+                'status' => 'error',
+                'message' => 'Erro ao remover reunião da base de dados.',
+                'title' => 'Erro ao remover.'
+            ];
+        }
+
+        return $response;
+    } catch (PDOException $e) {
+        // Rollback the transaction if there is an error
+        $conn->rollBack();
+        error_log('Error while deleting meeting: ' . $e->getMessage());
+    }
+}
---
+++ js/index.js
@@ -0,0 +1,223 @@
+let sessionUsername;
+// Meetings array
+const meetings = [];
+// Grab the add meeting button id
+const addMeetingBtn = document.getElementById('add-meeting-btn');
+// Grab root container that will be populated with the meetings
+const meetingsContainer = document.getElementById('meetings-container');
+// Grab theloading overlay div
+const loadingOverlay = document.getElementById('loading-overlay');
+// Event listener for add meeting button
+addMeetingBtn.addEventListener('click', () => goToAddEditMeetingPage(null));
+
+// This function will get current session username
+const getSessionUsername = () => {
+    loadingOverlay.style.display = 'block'; // Show loading overlay
+    $.get('api/index.php?action=get_username', (data, status) => {
+        if (status === 'success') {
+            // API call success
+            const parsedData = JSON.parse(data)
+            console.log('USERNAME =>', parsedData);
+            sessionUsername = parsedData
+            getMeetings();
+        } else {
+            // API call error
+            alert('Failed to fetch session username');
+        }
+    })
+}
+getSessionUsername(); // Grab the current session USERNAME via ajax call
+
+// Function to call api to fetch all meetings
+const getMeetings = () => {
+    $.get('api/index.php?action=get_meetings', (data, status) => {
+        if (status === 'success') {
+            // Api call success       
+            console.log(data) // Log result
+            const parsedData = JSON.parse(data); // Parse json data
+            parsedData.forEach(meeting => {
+                meetings.push(meeting); // Push parsed meeting data to meetings array
+            });
+            populateMeetingsContainer() // Populate meetings container
+        } else {
+            alert('Error fetching api data'); // Error fetching data from api
+        }
+    })
+}
+
+// Function to populate meetings container
+const populateMeetingsContainer = () => {
+    meetingsContainer.innerHTML = ''; // Clear meetingsContainer inner HTML
+
+    // Loop through meetings array and populate each meeting
+    meetings.forEach((meeting, index) => {
+        const divRow = document.createElement('div'); // Create div with row class (bootstrap)
+
+        // Condition to verify if session USERNAME matches meeting row 
+        const showEditDeleteButtons = sessionUsername === meeting.organizador;
+
+        // Formatted date and time
+        const formattedDate = moment(meeting.data).format('DD/MM/YYYY');
+        const formattedHoraInicio = moment(meeting.hora_inicio, 'HH:mm:ss').format('HH:mm');
+        const formattedHoraFim = moment(meeting.hora_fim, 'HH:mm:ss').format('HH:mm');
+
+        // If true, show edit/delete buttons
+        const editDeleteButtons = showEditDeleteButtons ? `
+            <div class='col-sm-12 mt-3'>
+                <button id='edit-meeting-${index}' class='btn btn-primary'>Editar</button>
+                <button id='delete-meeting-${index}' class='btn btn-danger'>Remover</button>
+            </div>
+        ` : '';
+
+        // Populate row inner HTML
+        divRow.innerHTML = `
+            <div class='card my-3 c-card'>
+                <div class='row g-0 align-items-center'>
+                    <div class='col-sm-12 col-md-4 text-center text-md-start'>
+                        <img src='${meeting.url_imagem}' class='img-fluid rounded meeting-image'/>
+                    </div>
+                    <div class='col-sm-12 col-md-8 text-center text-md-start'>
+                        <div class='card-body'>
+                            <div class='row align-items-center'>
+                                <div class='col-sm-12 col-md'>
+                                    <h3 class='card-title' style='color: #ed6337'>${meeting.motivo}</h3>
+                                    <p class='card-text'><strong>Data:</strong> ${formattedDate}</p>
+                                    <p class='card-text'><strong>Hora: </strong>${formattedHoraInicio}h - ${formattedHoraFim}h</p>
+                                    <p class='card-text'><strong>Sala:</strong> ${meeting.sala}</p>
+                                </div>
+                                ${editDeleteButtons}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+        meetingsContainer.appendChild(divRow); // Append row HTML to parent container
+
+        // Condition to add event listener (if aplicable) for each edit/delete button
+        if (showEditDeleteButtons) {
+            const editBtn = document.getElementById(`edit-meeting-${index}`);
+            const deleteBtn = document.getElementById(`delete-meeting-${index}`);
+
+            editBtn.addEventListener('click', () => goToAddEditMeetingPage(meeting));
+            deleteBtn.addEventListener('click', () => alertDelete(meeting));
+        }
+
+        loadingOverlay.style.display = 'none'; // Hide loading overlay
+    });
+}
+
+/**
+ * Function that will navigate to addEditMeeting page and pass meeting object to localstorage to grab
+ * meeting object and populate the other page form with the correct data.
+ * If no object is passed through this function, it will navigate to addEditMeeting.html
+ * to add a new meeting.
+ * @param {object} meeting 
+ */
+const goToAddEditMeetingPage = (meeting) => {
+    if (meeting) {
+        localStorage.setItem('selectedMeeting', JSON.stringify(meeting)); // Save meeting object to local storage
+    } else {
+        localStorage.removeItem('selectedMeeting'); // Remove localStorage object set if meeting object is null
+    }
+    localStorage.setItem('organizador', JSON.stringify(sessionUsername));
+    window.location.href = 'addEditMeeting.html'; // Navigate to add/edit meeting
+}
+
+/**
+ * Function that will handle alert delete meeting
+ * @param {object} meeting 
+ */
+const alertDelete = (meeting) => {
+    const meetingHtml = generateMeetingHtml(meeting); // Generate meeting data html to display on sweet alert
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+            confirmButton: 'btn btn-success ms-1',
+            cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+    })
+
+    swalWithBootstrapButtons.fire({
+        title: 'De certeza que quer apagar esta reunião?',
+        html: meetingHtml,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Sim, quero apagar!',
+        cancelButtonText: 'Não, cancelar!',
+        reverseButtons: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            deleteMeeting(meeting)
+                .then(apiResponse => {
+                    swalWithBootstrapButtons.fire({
+                        title: apiResponse.title,
+                        text: apiResponse.message,
+                        icon: apiResponse.status,
+                        showCancelButton: false,
+                        confirmButtonText: 'Ok',
+                    }).then((result) => {
+                        if(result.isConfirmed) {
+                            window.location.reload() // Reload page
+                        }
+                    });
+                })
+                .catch(error => console.error(error));
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+            swalWithBootstrapButtons.fire(
+                'Cancelado',
+                'A tua reunião está segura :)',
+                'error'
+            )
+        }
+    })
+}
+
+/**
+ * This function will make an api call to delete meeting by id
+ * @param {object} meeting 
+ * @returns promise to handle response
+ */
+const deleteMeeting = (meeting) => {
+    // Return a Promise
+    return new Promise((resolve, reject) => {
+        // Handle API logic here
+        $.ajax({
+            url: 'api/index.php',
+            type: 'DELETE',
+            data: {
+                action: 'delete_meeting',
+                meeting_id: meeting.id
+            },
+            success: (response) => {
+                // Resolve the Promise with the response data
+                resolve(JSON.parse(response));
+            },
+            error: (error) => {
+                console.error(error);
+                // Reject the Promise with the error
+                reject(error);
+            }
+        });
+    });
+};
+
+const generateMeetingHtml = (meeting) => {
+    // Formatted date and time
+    const formattedDate = moment(meeting.data).format('DD/MM/YYYY');
+    const formattedHoraInicio = moment(meeting.hora_inicio, 'HH:mm:ss').format('HH:mm');
+    const formattedHoraFim = moment(meeting.hora_fim, 'HH:mm:ss').format('HH:mm');
+
+    const html = `
+        <h3 style='color: #ed6337'>${meeting.motivo}</h3>
+        <p><strong>Data:</strong> ${formattedDate}</p>
+        <p><strong>Hora: </strong>${formattedHoraInicio}h - ${formattedHoraFim}h</p>
+        <p><strong>Sala:</strong> ${meeting.sala}</p>
+    `;
+
+    return html;
+}
+
+window.onbeforeunload = () => {
+    loadingOverlay.style.display = 'none'; // Hide the loading spinner before leaving page
+};
